78b6ee8c4b342069d584d333a21761056d8f5e2d
khonen-git <khonen.pro@gmail.com>
2025-08-04 21:21:14 +0200

    fix: Correction de l'erruer de typage

--- src/app/knowledge/mathematics/probability/distributions/continuous/normal/page.tsx
+++ src/app/knowledge/mathematics/probability/distributions/continuous/normal/page.tsx
@@ -2,10 +2,10 @@
 
 import * as React from 'react';
 import { KnowledgeLayout } from '@/components/layouts/KnowledgeLayout';
-import * as normalContent from '@/content/knowledge/mathematics/probability/distributions/continuous/normal.mdx';
+import Content from '@/content/knowledge/mathematics/probability/distributions/continuous/normal.mdx';
 
 export default function NormalDistributionPage() {
   return (
-    <KnowledgeLayout content={normalContent} />
+    <KnowledgeLayout><Content /></KnowledgeLayout>
   );
 } 
